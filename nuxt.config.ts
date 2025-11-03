1a774fd267fce08def2c64a8afd839bae742c311
xKaimac <admin@kaimac.com.au>
2025-11-03 19:56:49 +1030

    Typesafe env setup Fixes #1

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import './app/lib/env.ts';
 export default defineNuxtConfig({
-																compatibilityDate: '2025-07-15',
-																devtools: { enabled: true },
-																modules: [
-																 '@nuxt/eslint',
-																 '@nuxt/icon',
-																 '@nuxt/ui',
-																 '@nuxtjs/leaflet',
-																],
-																css: ['~/assets/css/main.css'],
+	compatibilityDate: '2025-07-15',
+	devtools: { enabled: true },
+	modules: ['@nuxt/eslint', '@nuxt/icon', '@nuxt/ui', '@nuxtjs/leaflet'],
+	css: ['~/assets/css/main.css'],
 });
